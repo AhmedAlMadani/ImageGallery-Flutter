656bf262dfcff6001c533032b63193182b6cffe8
AhmedAlMadani <ahmedmoeen87@gmail.com>
2021-12-24 18:57:42 +0200

    Final gallery Edit

---
+++ lib/Controller/image_upload_controller.dart
@@ -0,0 +1,157 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ImageUpload extends StatefulWidget {
+  final String? userId;
+  const ImageUpload({Key? key, this.userId}) : super(key: key);
+
+  @override
+  State<ImageUpload> createState() => _ImageUploadState();
+}
+
+class _ImageUploadState extends State<ImageUpload> {
+  // initializing some values
+  File? _image;
+  final imagePicker = ImagePicker();
+  String? downloadURL;
+
+  // picking the image
+
+  Future imagePickerMethod() async {
+    final pick = await imagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pick != null) {
+        _image = File(pick.path);
+      } else {
+        showSnackBar("No File selected", Duration(milliseconds: 400));
+      }
+    });
+  }
+
+  // uploading the image to firebase cloudstore
+  Future uploadImage(File _image) async {
+    final imgId = DateTime.now().millisecondsSinceEpoch.toString();
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    Reference reference = FirebaseStorage.instance
+        .ref()
+        .child('${widget.userId}/images')
+        .child("post_$imgId");
+
+    await reference.putFile(_image);
+    downloadURL = await reference.getDownloadURL();
+
+    // cloud firestore
+    await firebaseFirestore
+        .collection("users")
+        .doc(widget.userId)
+        .collection("images")
+        .add({'downloadURL': downloadURL}).whenComplete(
+            () => showSnackBar("Image Uploaded", Duration(seconds: 2)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Upload Image",
+          style: GoogleFonts.getFont('Sen',
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        backgroundColor: Colors.deepPurple[200],
+        foregroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: SizedBox(
+                    height: 500,
+                    width: double.infinity,
+                    child: Column(children: [
+                      Text("Upload Image",
+                          style: GoogleFonts.getFont('Sen',
+                              fontSize: 15,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black)),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Expanded(
+                        flex: 4,
+                        child: Container(
+                          width: 300,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.purple),
+                          ),
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                // the image that we wanted to upload
+                                Expanded(
+                                    child: _image == null
+                                        ? Center(
+                                            child: Text("No image selected",
+                                                style: GoogleFonts.getFont(
+                                                    'Sen',
+                                                    fontSize: 15,
+                                                    fontWeight:
+                                                        FontWeight.normal)))
+                                        : Image.file(_image!)),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    imagePickerMethod();
+                                  },
+                                  child: Text(
+                                    "Select Image",
+                                    style: GoogleFonts.getFont('Sen',
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.black),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                      primary: Colors.deepPurple[200]),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    if (_image != null) {
+                                      uploadImage(_image!);
+                                    } else {
+                                      showSnackBar("Select Image first",
+                                          Duration(milliseconds: 400));
+                                    }
+                                  },
+                                  child: Text(
+                                    "Upload Image",
+                                    style: GoogleFonts.getFont('Sen',
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.black),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                      primary: Colors.deepPurple[200]),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ])))),
+      ),
+    );
+  }
+
+  // show snack bar
+
+  showSnackBar(String snackText, Duration d) {
+    final snackBar = SnackBar(content: Text(snackText), duration: d);
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_image_gallery/view/gallery_screen.dart';
-import 'package:my_image_gallery/view/upload_screen.dart';
-import './view/HomePage.dart';
+import 'Screens/login_screen.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GalleryScreen(),
+      title: 'Email And Password Login',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 }
